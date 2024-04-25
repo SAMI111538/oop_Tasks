7d01fa496368419aed301575391e759d3a0bb142
Sami Ullah <120156873+Sami-143@users.noreply.github.com>
2024-04-25 13:02:00 +0500

    Add files via upload

---
+++ AddEmployee/AddEmployee/Program.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddEmployee
+{
+    class Program
+    {
+        class Product
+        {
+            public string pro_Name;
+            public int pro_Price;
+            public int pro_Stock;
+
+            public Product()
+            {
+
+            }
+            public Product(string pro_Name, int pro_Price, int pro_Stock)
+            {
+                this.pro_Name = pro_Name;
+                this.pro_Price = pro_Price;
+                this.pro_Stock = pro_Stock;
+            }
+        }
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>();
+        }
+
+
+            
+    }
+}
